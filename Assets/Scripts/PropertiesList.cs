4a6e9ea90f54000c36ba015828abc7e5dbaf91b0
Lachlan <Lachlan.d.wood@student.uts.edu.au>
2018-01-30 00:15:49 +1100

    Unity binary stuff changes
    Binary stuff does what binary stuff does ¯\_(ツ)_/¯

--- Assets/Scripts/PropertiesList.cs
+++ Assets/Scripts/PropertiesList.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PropertiesList {
 
-	public static Property[] properties = new Property[4] { new Property ("Coinage", "Gold", 0, 0, true), 
+	public static Property[] properties = new Property[4] {
+		new Property ("Coinage", "Gold", 0, 0, true), 
 		new Property ("Factory", "Production", 2, 16),
 		new Property ("Granary", "Food", 2, 12),
-		new Property ("Mine", "Gold", 4, 15) };
+		new Property ("Mine", "Gold", 4, 15)
+	};
 
 	public static Property getProperty(int position) {
 		return properties [position];
